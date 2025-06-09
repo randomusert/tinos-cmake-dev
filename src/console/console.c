54d9f4d6b0c18b3a88a67da4a273e709c0a7a02c
randomusert <eetu.pajamaki@edutornio.net>
2025-06-09 18:25:09 +0300

    blank drivers

--- src/console/console.c
+++ src/console/console.c
@@ -1,5 +1,5 @@
 #include "../include/text.h"
 
 void console() {
-    text("console>");
+    text("welcome to tinos cmake edition\nthis os is for the fun and is not really a prod os\nNOTE: this is a hobby os and not a commercial os\nconsole>");
 }
